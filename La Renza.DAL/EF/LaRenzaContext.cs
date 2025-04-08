084ecd5ba7b254fb8b427216e292a341269c82af
Lenovo <Lenovo@DESKTOP-1BR19GA>
2025-04-08 09:15:51 +0300

    //Создание скелета слоев многослойной архитектуры(примерно)

---
+++ La Renza.DAL/EF/LaRenzaContext.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+//using La_Renza.DAL.Entities;
+
+namespace La_Renza.DAL.EF
+{   
+    public class LaRenzaContext : DbContext
+    { 
+        public LaRenzaContext(DbContextOptions<LaRenzaContext> options)
+                   : base(options)
+        {
+            //Database.EnsureCreated();
+        }
+    }
+
+    //add-migration CreateDb
+    //update-database
+    public class SampleContextFactory : IDesignTimeDbContextFactory<LaRenzaContext>
+    {
+        public LaRenzaContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<LaRenzaContext>();
+
+
+            ConfigurationBuilder builder = new ConfigurationBuilder();
+            builder.SetBasePath(Directory.GetCurrentDirectory());
+            builder.AddJsonFile("appsettings.json");
+            IConfigurationRoot config = builder.Build();
+
+
+            string connectionString = config.GetConnectionString("DefaultConnection");
+            optionsBuilder.UseMySQL(connectionString);
+            return new LaRenzaContext(optionsBuilder.Options);
+        }
+    }
+}
