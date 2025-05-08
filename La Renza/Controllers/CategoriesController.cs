19a7dba8050fea7ccfdc3614c0a4a5281b6fc926
Maryprog <144013702+hrebenchukM@users.noreply.github.com>
2025-05-08 11:19:48 +0300

    //Оформить примерно контроллер Категорий и Заказов

--- La Renza/Controllers/CategoriesController.cs
+++ La Renza/Controllers/CategoriesController.cs
@@ -1,6 +1,7 @@
 ﻿using La_Renza.BLL.DTO;
 using La_Renza.BLL.Interfaces;
 using La_Renza.BLL.Services;
+using La_Renza.DAL.Entities;
 using Microsoft.AspNetCore.Mvc;
 
 namespace La_Renza.Controllers
@@ -19,32 +20,48 @@ namespace La_Renza.Controllers
           
         }
 
-
         // GET: api/Categories
         [HttpGet]
         public async Task<ActionResult<IEnumerable<CategoryDTO>>> GetCategories()
         {
-            return Ok(); 
+            var categories = await _categoryService.GetCategories();
+            return Ok(categories); 
         }
 
         // GET: api/Categories/5
         [HttpGet("{id}")]
         public async Task<ActionResult<CategoryDTO>> GetCategory(int id)
         {
-            return Ok();
+            CategoryDTO category = await _categoryService.GetCategory((int)id);
+
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return new ObjectResult(category);
         }    
         // PUT: api/Categories/5
         [HttpPut("{id}")]
         public async Task<IActionResult> PutCategory(int id, CategoryDTO category)
         {
-            return Ok();
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            await _categoryService.UpdateCategory(category);
+            return Ok(category);
         }
 
         // POST: api/Categories
         [HttpPost]
         public async Task<ActionResult<CategoryDTO>> PostCategory(CategoryDTO category)
         {
-            return Ok(); 
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            await _categoryService.CreateCategory(category);
+            return Ok(category);
         }
 
 
@@ -52,7 +69,20 @@ namespace La_Renza.Controllers
         [HttpDelete("{id}")]
         public async Task<IActionResult> DeleteCategory(int id)
         {
-            return Ok(); 
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            CategoryDTO category = await _categoryService.GetCategory((int)id);
+
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            await _categoryService.DeleteCategory(id);
+
+            return Ok(category);
         }
         /////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -60,35 +90,65 @@ namespace La_Renza.Controllers
         [HttpGet]
         public async Task<ActionResult<IEnumerable<SizeDTO>>> GetSizes()
         {
-            return Ok(); 
+            var sizes = await _sizeService.GetSizes();
+            return Ok(sizes);
         }
 
         // GET: api/Sizes/5
         [HttpGet("{id}")]
         public async Task<ActionResult<SizeDTO>> GetSize(int id)
         {
-            return Ok(); 
+            SizeDTO size = await _sizeService.GetSize((int)id);
+
+            if (size == null)
+            {
+                return NotFound();
+            }
+            return new ObjectResult(size);
         }
 
         // PUT: api/Sizes/5
         [HttpPut("{id}")]
         public async Task<IActionResult> PutSize(int id, SizeDTO size)
         {
-            return Ok(); 
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            await _sizeService.UpdateSize(size);
+            return Ok(size);
         }
 
         // POST: api/Sizes
         [HttpPost]
         public async Task<ActionResult<SizeDTO>> PostSize(SizeDTO size)
         {
-            return Ok(); 
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            await _sizeService.CreateSize(size);
+            return Ok(size);
         }
 
         // DELETE: api/Sizes/5
         [HttpDelete("{id}")]
         public async Task<IActionResult> DeleteSize(int id)
         {
-            return Ok(); 
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            SizeDTO size = await _sizeService.GetSize((int)id);
+
+            if (size == null)
+            {
+                return NotFound();
+            }
+
+            await _sizeService.DeleteSize(id);
+
+            return Ok(size);
         }
     }
 }
