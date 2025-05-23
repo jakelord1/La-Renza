cbdfab10d6ffc5679cc69aa832bec768a6220fec
jakelord1 <jakelord1.vo@gmail.com>
2025-05-23 13:00:18 +0300

    Hashing fixed

---
+++ La Renza.BLL/Infrastructure/PasswordHasher.cs
@@ -0,0 +1,17 @@
+﻿using BCrypt.Net;
+
+namespace La_Renza.BLL
+{
+    public class PasswordHasher
+    {
+        public string HashPassword(string password)
+        {
+            return BCrypt.Net.BCrypt.HashPassword(password);
+        }
+
+        public bool VerifyPassword(string password, string hash)
+        {
+            return BCrypt.Net.BCrypt.Verify(password, hash);
+        }
+    }
+}
