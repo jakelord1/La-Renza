c96527a0cdd525806415bc4c2251436bab0a631f
Maryprog <marya101204@gmail.com>
2025-05-24 17:04:50 +0300

    //

--- La Renza.BLL/Services/InvoiceInfoService.cs
+++ La Renza.BLL/Services/InvoiceInfoService.cs
@@ -20,6 +20,7 @@ namespace La_Renza.BLL.Services
         {
             var invoiceInfo = new InvoiceInfo
             {
+                Id = invoiceInfoDto.Id,
                 UserId = invoiceInfoDto.UserId,
                 FullName = invoiceInfoDto.FullName,
                 SecondName = invoiceInfoDto.SecondName,
@@ -37,6 +38,7 @@ namespace La_Renza.BLL.Services
         {
             var invoiceInfo = new InvoiceInfo
             {
+                Id = invoiceInfoDto.Id,
                 UserId = invoiceInfoDto.UserId,
                 FullName = invoiceInfoDto.FullName,
                 SecondName = invoiceInfoDto.SecondName,
@@ -63,6 +65,7 @@ namespace La_Renza.BLL.Services
                 throw new ValidationException("Wrong invoices!", "");
             return new InvoiceInfoDTO
             {
+                Id = invoiceInfoDto.Id,
                 UserId = invoiceInfo.UserId,
                 FullName = invoiceInfo.FullName,
                 SecondName = invoiceInfo.SecondName,
