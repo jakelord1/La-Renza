d0fe45fd5a1eac1502a06fdbae6c87b47d78b9ee
si4man <authorityexceeded@gmail.com>
2025-06-10 13:28:43 +0300

    changes in bll (remove comments)

--- La Renza.BLL/BLLServiceCollectionExtensions.cs
+++ La Renza.BLL/BLLServiceCollectionExtensions.cs
@@ -9,10 +9,8 @@ namespace La_Renza.BLL
     {
         public static IServiceCollection AddLaRenzaBLL(this IServiceCollection services)
         {
-            // Підключення сервісу для роботи з конфігурацією (json)
             var configPath = Path.Combine(Directory.GetCurrentDirectory(), "..", "La Renza.DAL", "Data", "configurator.json");
             services.AddSingleton<IConfiguratorService>(provider => new ConfiguratorService(configPath));
-            // Інші сервіси...
             return services;
         }
     }
