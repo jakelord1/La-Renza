893466d52a38c8f39cfbe0b3c85e7881a0ae33f5
Maryprog <marya101204@gmail.com>
2025-07-02 01:39:04 +0300

    фик

--- La Renza/Controllers/AccountController.cs
+++ La Renza/Controllers/AccountController.cs
@@ -398,25 +398,9 @@ namespace La_Renza.Controllers
                 return Unauthorized(new { message = "User not logged in." });
 
             var favoriteModels = await _productService.GetModelsByUserId(user.Id);
-            var result = favoriteModels.Select(model => new ModelProduct
-            {
-                Id = model.Id,
-                Name = model.Name ?? "Unknown",
-                Description = model.Description,
-                MaterialInfo = model.MaterialInfo,
-                StartDate = model.StartDate,
-                Price = (decimal)model.Price,
-                ImageUrl = model.Colors.FirstOrDefault()?.Image?.Path ?? "",
-                Rate = model.Rate,
-                Sizes = model.Sizes,
-                CategoryId = model.CategoryId,
-                Bages = !string.IsNullOrEmpty(model.Bage)
-                  ? new List<string> { model.Bage! }
-                  : new List<string>()
+            
 
-            }).ToList();
-
-            return Ok(result);
+            return Ok(favoriteModels);
         }
 
         // GET: api/Account/
